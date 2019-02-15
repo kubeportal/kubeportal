ac9b4abeb298b4678f45d0779a136baa89727d3b
Martin v. Löwis <loewis@beuth-hochschule.de>
2019-02-15 17:54:53 +0100

    Reenable social_user pipeline step.

--- kubeportal/settings.py
+++ kubeportal/settings.py
@@ -115,6 +115,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_details',
     'social_core.pipeline.social_auth.social_uid',
     'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.user.create_user',
     'social_core.pipeline.social_auth.associate_user',
