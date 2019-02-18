6fc036b1b47b2f9f332598bfcfc78333d0d10610
Peter Tröger <peter@troeger.eu>
2019-02-18 21:40:31 +0100

    Use Django LoginView

--- kubeportal/urls.py
+++ kubeportal/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
 from kubeportal import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name="index"),
+    path('', LoginView.as_view(template_name='index.html', redirect_authenticated_user=True), name="index"),
     path('dashboard', views.DashboardView.as_view(), name="dashboard"),
     path('logout', LogoutView.as_view(), name="logout"),
     path('social/', include('social_django.urls')),
--- kubeportal/views.py
+++ kubeportal/views.py
@@ -1,17 +1,5 @@
 from django.views.generic.base import TemplateView
-from django.shortcuts import redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
-
-
-class IndexView(TemplateView):
-
-    template_name = "index.html"
-
-    def get(self, request):
-        if request.user.is_authenticated:
-            return redirect('dashboard')
-        else:
-            return super(IndexView, self).get(request)
 
 
 class DashboardView(LoginRequiredMixin, TemplateView):
