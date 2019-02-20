10f675c620c994423cb01149ca2024dd5b402b6b
Peter Tröger <peter@troeger.eu>
2019-02-20 10:55:06 +0100

    Separate view for Fernet token stuff

--- kubeportal/views.py
+++ kubeportal/views.py
@@ -6,12 +6,8 @@ from .token import FernetToken, InvalidToken
 from kubeportal.models import ClusterApplication
 
 
-class DashboardView(LoginRequiredMixin, TemplateView):
-    template_name = "dashboard_index.html"
-
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context['clusterapps'] = ClusterApplication.objects.all()
+class FernetTokenView(LoginRequiredMixin, TemplateView):
+    template_name = "dashboard_fernet.html"
 
     def post(self, request):
         context = self.get_context_data()
@@ -35,9 +31,14 @@ class DashboardView(LoginRequiredMixin, TemplateView):
         context['verify_msg'] = verify_msg
         return self.render_to_response(context)
 
-# XXX parameters to be configured:
-server_url = 'TBD'
-clustername = 'TBD'
+
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = "dashboard_index.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['clusterapps'] = ClusterApplication.objects.all()
+        return context
 
 
 class ConfigDownloadView(LoginRequiredMixin, TemplateView):
@@ -47,10 +48,6 @@ class ConfigDownloadView(LoginRequiredMixin, TemplateView):
         context = super().get_context_data(**kwargs)
         username = self.request.user.username
         context['username'] = username
-        context['server_url'] = server_url
-        context['clustername'] = clustername
-        fernet = FernetToken()
-        context['token'] = fernet.username_to_token(username)
         return context
 
     def render_to_response(self, context, **response_kwargs):
@@ -61,25 +58,3 @@ class ConfigDownloadView(LoginRequiredMixin, TemplateView):
 
 class ConfigView(LoginRequiredMixin, TemplateView):
     template_name = "dashboard_config.html"
-
-    def post(self, request):
-        context = self.get_context_data()
-        token = self.request.POST.get('token')
-        fernet = FernetToken()
-        verify_msg = ''
-        if token:
-            context['token'] = token       # reinsert into form
-            # handle form post for token verification
-            try:
-                username = fernet.token_to_username(token)
-            except InvalidToken:
-                verify_msg = 'This token is invalid'
-            else:
-                if username == self.request.user.username:
-                    stamp = fernet.extract_timestamp(token)
-                    stamp = time.ctime(stamp)
-                    verify_msg = 'This is your token, issued ' + stamp
-                else:
-                    verify_msg = "This is the token of somebody else's token"
-        context['verify_msg'] = verify_msg
-        return self.render_to_response(context)
