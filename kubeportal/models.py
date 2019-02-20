b3294fceaf504a9065f1a361350e005df07e72d6
Peter Tröger <peter@troeger.eu>
2019-02-20 10:13:00 +0100

    Re-design model

--- kubeportal/models.py
+++ kubeportal/models.py
@@ -2,6 +2,25 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class ServiceAccount(models.Model):
+class KubernetesNamespace(models.Model):
+    '''
+    A replication of namespaces known to the API server.
+    '''
     name = models.CharField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class KubernetesServiceAccount(models.Model):
+    '''
+    A replication of service accounts known to the API server.
+    '''
+    name = models.CharField(max_length=100)
+    namespace = models.ForeignKey(KubernetesNamespace, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+
+class ClusterApplication(models.Model):
+    '''
+    Cluster applications to be shown in the frontend.
+    '''
+    name = models.CharField(max_length=100)
+    url = models.URLField()
