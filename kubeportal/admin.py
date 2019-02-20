c7b8e1df8e65e67f3747372890ff0d4b9b328e8a
Peter Tröger <peter@troeger.eu>
2019-02-20 10:13:19 +0100

    Restrict permissions on cached stuff (incomplete)

--- kubeportal/admin.py
+++ kubeportal/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
 from django.conf import settings
 from django.contrib.auth.models import User, Group
-from .models import ServiceAccount
+from .models import KubernetesServiceAccount, KubernetesNamespace, ClusterApplication
 
-admin_site = admin.AdminSite()
+
+class RestrictedAdmin(admin.AdminSite):
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+
+admin_site = RestrictedAdmin()
 admin_site.site_header = settings.BRANDING + ' - Administration'
 admin_site.register(User)
 admin_site.register(Group)
-admin_site.register(ServiceAccount)
+admin_site.register(KubernetesServiceAccount)
+admin_site.register(KubernetesNamespace)
+admin_site.register(ClusterApplication)
