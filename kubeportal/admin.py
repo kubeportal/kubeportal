ba3be1f3706dabbf2dd6c235a628b367c6060d0f
Peter Tröger <peter@troeger.eu>
2019-02-20 11:23:21 +0100

    Truly restrict admin access to cached stuff

--- kubeportal/admin.py
+++ kubeportal/admin.py
@@ -3,8 +3,17 @@ from django.conf import settings
 from django.contrib.auth.models import User, Group
 from .models import KubernetesServiceAccount, KubernetesNamespace, ClusterApplication
 
+class KubernetesServiceAccountAdmin(admin.ModelAdmin):
+    def has_change_permission(self, request, obj=None):
+        return False
 
-class RestrictedAdmin(admin.AdminSite):
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+class KubernetesNamespaceAdmin(admin.ModelAdmin):
     def has_change_permission(self, request, obj=None):
         return False
 
@@ -15,10 +24,11 @@ class RestrictedAdmin(admin.AdminSite):
         return False
 
 
-admin_site = RestrictedAdmin()
+
+admin_site = admin.AdminSite()
 admin_site.site_header = settings.BRANDING + ' - Administration'
 admin_site.register(User)
 admin_site.register(Group)
-admin_site.register(KubernetesServiceAccount)
-admin_site.register(KubernetesNamespace)
+admin_site.register(KubernetesServiceAccount, KubernetesServiceAccountAdmin)
+admin_site.register(KubernetesNamespace, KubernetesNamespaceAdmin)
 admin_site.register(ClusterApplication)
