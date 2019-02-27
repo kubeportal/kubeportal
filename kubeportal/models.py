b4ee398cb0adcd2d63f6c1456b0f69df16b38b0b
Peter Tröger <peter@troeger.eu>
2019-02-27 09:05:36 +0100

    Better help texts.

--- kubeportal/models.py
+++ kubeportal/models.py
@@ -27,19 +27,22 @@ class KubernetesNamespace(models.Model):
     '''
     A replication of namespaces known to the API server.
     '''
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, help_text="Lower case alphanumeric characters or '-', and must start and end with an alphanumeric character (e.g. 'my-name', or '123-abc').")
     uid = models.CharField(max_length=50, null=True, editable=False)
-    visible = models.BooleanField(default=True, help_text='Visibility of the namespace in admin interface')
+    visible = models.BooleanField(default=True, help_text='Visibility in admin interface. Can only be configured by a superuser.')
 
     def __str__(self):
         return self.name
+
+    def is_synced(self):
+        return self.uid is not None
 
 
 class KubernetesServiceAccount(models.Model):
     '''
     A replication of service accounts known to the API server.
     '''
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, help_text="Lower case alphanumeric characters or '-', and must start and end with an alphanumeric character (e.g. 'my-name', or '123-abc').")
     uid = models.CharField(max_length=50, null=True, editable=False)
     namespace = models.ForeignKey(
         KubernetesNamespace, on_delete=models.CASCADE)
