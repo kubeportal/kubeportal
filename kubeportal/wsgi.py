3ee7e3ddd65aff6dd5cfa5ac9ccf6d2648a87fa8
Peter Tröger <peter@troeger.eu>
2019-02-27 17:00:38 +0100

    Fix WSGI setup

--- kubeportal/wsgi.py
+++ kubeportal/wsgi.py
@@ -1,7 +1,8 @@
 import os
-from configurations.wsgi import get_wsgi_application
 
+# Order is crucial here!
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kubeportal.settings')
 os.environ.setdefault('DJANGO_CONFIGURATION', 'Production')
 
+from configurations.wsgi import get_wsgi_application
 application = get_wsgi_application()
