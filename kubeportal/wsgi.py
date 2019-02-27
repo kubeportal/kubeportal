67a7f12d6795a7d04849126fcb5c09447a95c1d7
Peter Tröger <peter@troeger.eu>
2019-02-27 10:54:56 +0100

    Use configurations everywhere

--- kubeportal/wsgi.py
+++ kubeportal/wsgi.py
@@ -1,7 +1,7 @@
 import os
-
-from django.core.wsgi import get_wsgi_application
+from configurations.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kubeportal.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Production')
 
 application = get_wsgi_application()
