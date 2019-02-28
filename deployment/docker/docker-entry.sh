0eea95ffd25e331d2d49772caa907b9c04b20e26
Peter Tröger <peter@troeger.eu>
2019-02-28 16:08:35 +0100

    New approach for generating superuser

--- deployment/docker/docker-entry.sh
+++ deployment/docker/docker-entry.sh
@@ -2,8 +2,7 @@
 set -e
 
 /code/manage.py migrate --configuration=Production
-
-echo "from django.contrib.auth import get_user_model; get_user_model().objects.create_superuser('admin', 'myemail@example.com', 'admin')" | python /code/manage.py shell --configuration=Production
+/code/manage.py ensure_root --configuration=Production
 
 /usr/local/bin/uwsgi --http-auto-chunked \
                      --http-keepalive \
