62bed0d4c7e8b41041857e67b3bbda445f22a2be
Peter Tröger <peter@troeger.eu>
2019-08-13 16:15:43 +0200

    Tolerate URL mistakes for auth, to avoid 500 cases for arbitrary applications

--- kubeportal/urls.py
+++ kubeportal/urls.py
@@ -7,6 +7,7 @@ from kubeportal.admin import admin_site
 urlpatterns = [
     path('config/', views.ConfigView.as_view(), name='config'),
     path('subauthreq/', views.SubAuthRequestView.as_view(), name='subauthreq'),
+    path('subauthreq', views.SubAuthRequestView.as_view(), name='subauthreq'),
     path('config/download/', views.ConfigDownloadView.as_view(content_type='text/plain'), name='config_download'),
     path('', views.IndexView.as_view(), name="index"),
     path('welcome/', views.WelcomeView.as_view(), name="welcome"),
