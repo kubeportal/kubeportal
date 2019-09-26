03578664b849ed61dea1bb99816c85ee2bbf4756
Leonard Lorenz <bi0m3tr1xx@gmail.com>
2019-09-26 11:02:27 +0200

    switched back to filtering only approved users

--- kubeportal/api/views.py
+++ kubeportal/api/views.py
@@ -3,12 +3,12 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 
 from kubeportal.api.serializers import UserSerializer
-from kubeportal.models import User, UserState
+from kubeportal.models import UserState
 from django.contrib.auth import get_user_model
 
 '''
 API endpoint that allows for users to queried
 '''
 class UserView(ModelViewSet, ListModelMixin):
-    queryset = get_user_model().objects.all() #.filter(state=UserState.ACCESS_APPROVED)
+    queryset = get_user_model().objects.filter(state=UserState.ACCESS_APPROVED)
     serializer_class = UserSerializer
