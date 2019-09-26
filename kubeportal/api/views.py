864f5af6ac182e1fe734c61bf40faa6b0c80aaac
Leonard Lorenz <bi0m3tr1xx@gmail.com>
2019-09-26 10:59:22 +0200

    Fixed User model access, removed django-filter from the requirements, Added make api-user and make api-token

--- kubeportal/api/views.py
+++ kubeportal/api/views.py
@@ -1,20 +1,14 @@
-from django.views.decorators.csrf import csrf_exempt
-
 from rest_framework.mixins import ListModelMixin
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from rest_framework.views import View
-import json
-
 from rest_framework.response import Response
 
 from kubeportal.api.serializers import UserSerializer
 from kubeportal.models import User, UserState
+from django.contrib.auth import get_user_model
 
 '''
 API endpoint that allows for users to queried
 '''
 class UserView(ModelViewSet, ListModelMixin):
-    queryset = User.objects.all() #.filter(state=UserState.ACCESS_APPROVED)
+    queryset = get_user_model().objects.all() #.filter(state=UserState.ACCESS_APPROVED)
     serializer_class = UserSerializer
