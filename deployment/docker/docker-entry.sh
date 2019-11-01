2e1e44289cbcd8f74e420c25f703d7e007880107
Leonard Lorenz <bi0m3tr1xx@gmail.com>
2019-11-01 10:42:05 +0100

    docker-entry changes to accomodate token creation

--- deployment/docker/docker-entry.sh
+++ deployment/docker/docker-entry.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+ip addr | grep inet | grep brd | sed 's/  //g'
+
 /code/manage.py migrate --configuration=Production
 /code/manage.py creatersakey --configuration=Production
 /code/manage.py ensure_root --configuration=Production
