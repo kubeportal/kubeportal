a521db0e6f707bc075248e3beb2073f4bcf79c8b
Peter Tröger <peter@troeger.eu>
2019-11-01 10:51:15 +0100

    Prettify env variable names, fix deployment examples

--- deployment/docker/docker-entry.sh
+++ deployment/docker/docker-entry.sh
@@ -5,8 +5,8 @@ ip addr | grep inet | grep brd | sed 's/  //g'
 /code/manage.py migrate --configuration=Production
 /code/manage.py creatersakey --configuration=Production
 /code/manage.py ensure_root --configuration=Production
-/code/manage.py createsuperuser --username $API_USER_USERNAME --email $API_USER_EMAIL --noinput --configuration=Production
-/code/manage.py drf_create_token $API_USER_USERNAME --configuration=Production
+/code/manage.py createsuperuser --username $KUBEPORTAL_API_USER_USERNAME --email $KUBEPORTAL_API_USER_EMAIL --noinput --configuration=Production
+/code/manage.py drf_create_token $KUBEPORTAL_API_USER_USERNAME --configuration=Production
 
 /usr/local/bin/uwsgi --http-auto-chunked \
                      --http-keepalive \
