85284f231ff2dec4c8c058e4143c13584cd0e5aa
Peter Tröger <peter@troeger.eu>
2019-11-05 15:33:37 +0100

    Bump version: 0.1.23 → 0.2.0

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 SHELL = /bin/bash
-VERSION = 0.1.23
+VERSION = 0.2.0
 
 .PHONY: check-venv
 
--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 project = 'KubePortal'
 copyright = '2019, Peter Tröger'
 author = 'Peter Tröger'
-release = 'v0.1.23'
+release = 'v0.2.0'
 extensions = [
 ]
 templates_path = ['_templates']
