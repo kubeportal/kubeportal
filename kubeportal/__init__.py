766f9edf10016007fafeb6560fd1f1b0fdb4f0be
Leonard Lorenz <dev@leonardlorenz.de>
2020-02-03 16:05:13 +0100

    Added stdout handler to logging, added some inline doc

--- kubeportal/__init__.py
+++ kubeportal/__init__.py
@@ -1,1 +1,11 @@
+import logging, sys
+
+logger = logging.getLogger('KubePortal')
+
+handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(handler)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
 default_app_config = 'kubeportal.apps.KubePortalConfig'
