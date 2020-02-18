b5e8a97f7f611f6592cbd326f96081066c722e3a
Peter Tröger <peter@troeger.eu>
2020-02-18 21:44:53 +0100

    Bump version: 0.2.2 → 0.2.3

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 SHELL=/bin/bash
-VERSION=0.2.2
+VERSION=0.2.3
 
 .PHONY: docs
 
--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 project = 'KubePortal'
 copyright = '2019, Peter Tröger'
 author = 'Peter Tröger'
-release = 'v0.2.2'
+release = 'v0.2.3'
 extensions = [
 	'sphinx_issues'
 ]
