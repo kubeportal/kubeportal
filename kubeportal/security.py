dde57ee0a561508bfcba9ace99c8b48cade7b498
Peter Troeger <peter@troeger.eu>
2020-03-21 20:53:45 +0100

    Play with hooks

--- kubeportal/security.py
+++ kubeportal/security.py
@@ -3,6 +3,12 @@ import logging
 logger = logging.getLogger('KubePortal')
 
 
-def oidc_hook(id_token, user, token, request, **kwargs):
-    logger.debug("OIDC user info requested for {0}".format(user))
+def oidc_idtoken_hook(id_token, user, token, request, **kwargs):
+    logger.debug("OIDC ID token request for {0}".format(user))
+    import pdb; pdb.set_trace()
     return id_token
+
+def oidc_login_hook(request, user, client):
+    logger.debug("OIDC login request for {0}".format(user))
+    import pdb; pdb.set_trace()
+    return None
