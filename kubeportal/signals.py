35f0dd3f9885a6cf52d2a4379e59a610ebc39df4
Peter Troeger <peter@troeger.eu>
2020-03-26 21:09:46 +0100

    Comments

--- kubeportal/signals.py
+++ kubeportal/signals.py
@@ -12,6 +12,13 @@ logger = logging.getLogger('KubePortal')
 
 @receiver(post_save, sender=User)
 def auto_add_portal_groups(sender, instance, **kwargs):
+    '''
+    Portal groups have a flag for new users being added automatically.
+    One use case is a "all users" group.
+    This signal handlers manages the addition of new users to this group.
+    This works regardless of any other permissions. It also runs on every login,
+    since the last login date is updated.
+    '''
     for group in PortalGroup.objects.all():
         if group.auto_add:
             logger.debug("Automatically adding user {0} to group {1}".format(instance, group))
