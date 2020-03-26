4826e6d1a3bb3a126844cae1c945ba9e60f974e9
Peter Troeger <peter@troeger.eu>
2020-03-26 22:17:33 +0100

    Just use default permissions, rely on programmatic ModelAdmin right handling instead. Closes #52.

--- kubeportal/signals.py
+++ kubeportal/signals.py
@@ -9,7 +9,6 @@ import logging
 
 logger = logging.getLogger('KubePortal')
 
-
 @receiver(post_save, sender=User)
 def auto_add_portal_groups(sender, instance, **kwargs):
     '''
@@ -26,58 +25,4 @@ def auto_add_portal_groups(sender, instance, **kwargs):
             group.save()
 
 
-@receiver(post_save, sender=User)
-def create_permission_groups(sender, instance, **kwargs):
-    '''
-    Django permissions are created long after the inital
-    migration and the createsuperuser run. These reasons
-    for that are kind of fuzzy, but this leads to the fact
-    that this signal handler silently fails when createsuperuser
-    is used directly after the installion, due to missing permission
-    objects.
 
-    It does not fail, however, later when the first real user
-    is created, either through Python Social or an admin
-    activity in the backend. And this is good enough, the superuser
-    does not need permissions anyway.
-    '''
-    # Make sure the default permission group exists
-    admin_group, created = Group.objects.get_or_create(
-        name='Account Administrators')
-
-    # Reflect staff status in permission group membership, so that group
-    # permissions regulate the backend access details
-    if instance.is_staff:
-        logger.debug("Automatically adding backend permissions for user {0}".format(instance))
-        instance.groups.add(admin_group)
-    else:
-        logger.debug("Automatically removing backend permissions for user {0}".format(instance))
-        instance.groups.remove(admin_group)
-
-    # Set all permissions for the default permission group
-    try:
-        for perm_name in ['Can add user',
-                          'Can change user',
-                          'Can delete user',
-                          'Can add OAuth2 Application',
-                          'Can change OAuth2 Application',
-                          'Can view OAuth2 Application',
-                          'Can delete OAuth2 Application',
-                          'Can add kubernetes namespace',
-                          'Can change kubernetes namespace',
-                          'Can view kubernetes namespace',
-                          'Can add kubernetes service account',
-                          'Can change kubernetes service account',
-                          'Can view kubernetes service account',
-                          'Can add link',
-                          'Can change link',
-                          'Can delete link',
-                          'Can add Client',
-                          'Can change Client',
-                          'Can delete Client',
-                          'Can view Client']:
-            perm = Permission.objects.get(name=perm_name)
-            admin_group.permissions.add(perm)
-        admin_group.save()
-    except Exception:
-        pass
