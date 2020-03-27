78f49e85ad3342f229206e23119b74bc9adcecbc
Peter Troeger <peter@troeger.eu>
2020-03-27 22:50:33 +0100

    Working on signal handlers

--- kubeportal/signals.py
+++ kubeportal/signals.py
@@ -12,28 +12,36 @@ logger = logging.getLogger('KubePortal')
 
 @receiver(post_save, sender=User)
 def handle_user_change(sender, instance, **kwargs):
-    user_in_auto_admin_group = False
+    member_of_auto_admin_group = False
     for group in PortalGroup.objects.all():
         if group.auto_add:
             logger.debug("Making sure that user {0} is in auto-add group {1}".format(instance, group))
             group.members.add(instance)
             group.save()
-        if group.auto_admin and not instance.is_staff:
-            logger.info("Enabling missing admin rights for user {0} due to user change, based on membership in auto-admin group {1}".format(instance, group))
+        if group.auto_admin and group.has_member(instance) and not instance.is_staff:
+            logger.debug("Enabling admin rights for user {0} due to group membership in {1}".format(instance, group))
             instance.is_staff = True
             instance.save()
-            user_in_auto_admin_group = True
-    if not user_in_auto_admin_group and instance.is_staff:
-        logger.info("Removing admin rights for user {0}, no membership in auto-admin group found.".format(instance))
+            member_of_auto_admin_group = True
+    if not member_of_auto_admin_group and instance.is_staff:
+        logger.debug("Disabling admin rights for user {0}, not in any auto-admin group".format(instance))
         instance.is_staff = False
         instance.save()
 
 @receiver(post_save, sender=PortalGroup)
 def handle_group_change(sender, instance, **kwargs):
-    for user in User.objects.all():
-        if instance.auto_admin:
-            if instance.has_member(user) and not user.is_staff:
-                logger.info("Enabling missing admin rights for user {0} due to group change, based on membership in auto-admin group {1}".format(user, instance))
-        else:
-            # check if users exist that have no auto-admin group anymore, remove flag from them
+    for member in instance.members.all():
+        if instance.auto_admin and not member.is_staff:
+            logger.debug("Enabling admin rights for user {0} due to group membership in {1}".format(instance, group))
+            instance.is_staff = True
+            instance.save()
+        if not instance.auto_admin and member.is_staff:
+            found_another_auto_admin_group = False
+            for group in PortalGroup.objects.all():
+                if group.auto_admin and group.pk != instance.pk:
+                    found_another_auto_admin_group = True
+            if not found_another_auto_admin_group:
+                logger.debug("Disabling admin rights for user {0}, no longer in any auto-admin group".format(instance))
+                instance.is_staff = False
+                instance.save()
 
