cea68a279b56380451f0df4f7fa9837a6fcfb8b4
Peter Tröger <peter@troeger.eu>
2020-08-16 23:52:33 +0200

    Fix tests after allauth transition

--- kubeportal/tests/test_front_unappr.py
+++ kubeportal/tests/test_front_unappr.py
@@ -14,7 +14,7 @@ class FrontendLoggedInNotApproved(AdminLoggedInTestCase):
     def test_index_view(self):
         # User is already logged in, expecting welcome redirect
         response = self.c.get('/')
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 301)
 
     def test_welcome_view(self):
         response = self.c.get('/welcome/')
@@ -34,11 +34,11 @@ class FrontendLoggedInNotApproved(AdminLoggedInTestCase):
 
     def test_root_redirect_with_next_param(self):
         response = self.c.get('/?next=/config')
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 301)
 
     def test_root_redirect_with_rd_param(self):
         response = self.c.get('/?next=/config')
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 301)
 
     def test_subauth_view(self):
         group1 = PortalGroup()
@@ -51,11 +51,6 @@ class FrontendLoggedInNotApproved(AdminLoggedInTestCase):
         response = self.c.get('/subauthreq/{}/'.format(app1.pk))
 
         self.assertEqual(response.status_code, 401)
-
-    def test_logout_view(self):
-        # User is already logged in, expecting redirect
-        response = self.c.get('/logout/')
-        self.assertEqual(response.status_code, 302)
 
     def test_acess_request_view(self):
         response = self.c.get('/access/request/')
