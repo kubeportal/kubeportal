0aba37b2b56885d19a8530782138ef89b096b7a2
Katharina Sachs <katharina.sachs@beuth-hochschule.de>
2020-08-25 20:05:43 +0200

    Squashed 'kubeportal-api/' changes from 9725390..eb99a9a
    eb99a9a Fix compat to latest API spec
    3ac65e1 Update README for new architecture
    ab5860b Add tests for statistics API, cleanup
    98c9c10 Fix URLs, add web applications to API
    42c16d5 Implement list of available statistics in API
    d234dbf Fix comments
    11dc8cc Implement statistics API, fetch data individually
    72e4fc0 Introduce API tests
    dd5c639 Fix test suite redirect type
    b9ac37b JWT, better logging
    000c988 Merge branch 'master' into fe-api
    d5dc51b Implemented user merging. Relevant for #59 (#73)
    
    git-subtree-dir: kubeportal-api
    git-subtree-split: eb99a9a62bfe25067069666c49c25ca6ed4fa018

--- kubeportal/urls.py
+++ kubeportal/urls.py
@@ -7,13 +7,14 @@ from allauth.socialaccount.providers.google import views as allauth_google_views
 from dj_rest_auth import views as dj_rest_views
 
 from kubeportal import views
-from kubeportal.admin import admin_site
-from kubeportal.views import GoogleApiLoginView
-
 from kubeportal.api import views as api_views
+from kubeportal.admin import admin_site
+
 from rest_framework import routers
 router = routers.SimpleRouter()
-router.register('users', api_views.UserView)
+router.register('users', api_views.UserView, basename='users')
+router.register('statistics', api_views.StatisticsView, basename='statistics')
+router.register('webapps', api_views.WebApplicationView, basename='webapplications')
 
 urlpatterns = [
     # frontend web views
@@ -36,11 +37,9 @@ urlpatterns = [
     path('.well-known/openid-configuration', ProviderInfoView.as_view(), name='provider_info'),
 
     # frontend API views
-    path('api/auth/login/', dj_rest_views.LoginView.as_view(), name='rest_login'),
-    path('api/auth/logout/', dj_rest_views.LogoutView.as_view(), name='rest_logout'),
-
-    path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('api/auth/google/login/', GoogleApiLoginView.as_view(), name='api_google_login'),
+    path('api/login', dj_rest_views.LoginView.as_view(), name='rest_login'),
+    path('api/logout', dj_rest_views.LogoutView.as_view(), name='rest_logout'),
+    path('api/login_google', views.GoogleApiLoginView.as_view(), name='api_google_login'),
     path('api/', include(router.urls), name='api'),
 
     # frontend web auth views
