38d21b5cba246651bb727889f9c642a13b90041f
Peter Troeger <peter@troeger.eu>
2020-09-01 17:11:36 +0200

    Re-use login GET parameters for redirect after login

--- kubeportal/allauth.py
+++ kubeportal/allauth.py
@@ -6,8 +6,15 @@ logger = logging.getLogger('KubePortal')
 class AccountAdapter(DefaultAccountAdapter):
 
     def get_login_redirect_url(self, request):
+        '''
+        The login template stores all incoming GET parameters in hidden POST fields.
+        We take them here and convert them back to GET parameters for the redirect.
+        '''
         if 'next' in request.POST:
-            url = request.POST['next']
+            url = request.POST['next'] + '?'
+            for key, value in request.POST.items():
+                if key not in ['next', 'login', 'password', 'csrfmiddlewaretoken']:
+                    url += "{}={}&".format(key, value)
             logger.info("Redirecting to {} due to 'next' POST parameter.".format(url))
             return url
         else:
