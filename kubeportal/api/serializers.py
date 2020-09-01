36a48fa9c9967dbd8ee963134b556db34b4ccb45
Peter Troeger <peter@troeger.eu>
2020-09-01 16:34:03 +0200

    Start to implement v1.1.0 API user format

--- kubeportal/api/serializers.py
+++ kubeportal/api/serializers.py
@@ -4,9 +4,26 @@ from kubeportal.models import WebApplication
 
 
 class UserSerializer(serializers.ModelSerializer):
+    firstname = serializers.CharField(source='first_name')
+    name = serializers.CharField(source='last_name')
+    primary_email = serializers.EmailField(source='email')
+    admin = serializers.BooleanField(source='is_staff')
+    all_emails = serializers.ListField(source='alt_mails')
+    k8s_account = serializers.CharField(source='service_account')
+    k8s_namespace = serializers.CharField()
+    k8s_token = serializers.CharField(source='token')
+
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username', 'first_name', 'last_name', 'email')
+        fields = (  'firstname',
+                    'name',
+                    'primary_email',
+                    'admin',
+                    'all_emails',
+                    'k8s_account',
+                    'k8s_namespace',
+                    'k8s_token',
+                    'portal_groups')
 
 
 class WebApplicationSerializer(serializers.ModelSerializer):
