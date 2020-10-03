4b1fcb2b42fbeec5e499bfd31b33418507aeae84
kat-hi <s77518@beuth-hochschule.de>
2020-10-03 10:51:53 +0200

    add allowed header field: Authorization

--- kubeportal/middleware.py
+++ kubeportal/middleware.py
@@ -89,5 +89,8 @@ class CorsMiddleware:
         response["Vary"] = "Origin"  # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
         response["Access-Control-Allow-Methods"] = "GET,HEAD,OPTIONS,POST,PUT"
         response["Access-Control-Allow-Credentials"] = "true"
-        response["Access-Control-Allow-Headers"] = "Access-Control-Allow-Origin, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+        response["Access-Control-Allow-Headers"] = "Access-Control-Allow-Origin, Access-Control-Allow-Headers, " \
+                                                   "Origin,Accept, X-Requested-With, Content-Type, " \
+                                                   "Access-Control-Request-Method, Access-Control-Request-Headers," \
+                                                   "Authorization"
         return response
