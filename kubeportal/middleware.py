1c101fd1aa3294e75fd7bc44e55805e8e3b89820
Peter Troeger <peter@troeger.eu>
2020-10-08 10:16:50 +0200

    Allow CSRF header, instead of cookie

--- kubeportal/middleware.py
+++ kubeportal/middleware.py
@@ -92,5 +92,5 @@ class CorsMiddleware:
         response["Access-Control-Allow-Headers"] = "Access-Control-Allow-Origin, Access-Control-Allow-Headers, " \
                                                    "Origin,Accept, X-Requested-With, Content-Type, " \
                                                    "Access-Control-Request-Method, Access-Control-Request-Headers," \
-                                                   "Authorization"
+                                                   "Authorization, X-CSRFToken"
         return response
