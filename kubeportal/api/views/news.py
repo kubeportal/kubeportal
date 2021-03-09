b99e9503fdb2aeb7281eccc67ce4b72db62153de
Deniz Cankurtaran <58952375+DenizCankurtaran@users.noreply.github.com>
2021-03-09 10:32:54 +0100

    adjusted news (#148)

--- kubeportal/api/views/news.py
+++ kubeportal/api/views/news.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers, generics
+from rest_framework.reverse import reverse
 
 from kubeportal.models.news import News
 
@@ -7,6 +8,13 @@ class NewsSerializer(serializers.ModelSerializer):
     class Meta:
         model = News
         fields = ['title', 'content', 'author', 'created', 'modified', 'priority']
+
+    def to_representation(self, data):
+        data = super(NewsSerializer, self).to_representation(data)
+        request = self.context['request']
+        author_url = reverse(viewname='user', kwargs={'user_id': data['author']}, request=request)
+        data['author'] = author_url
+        return data
 
 
 class NewsView(generics.ListAPIView):
