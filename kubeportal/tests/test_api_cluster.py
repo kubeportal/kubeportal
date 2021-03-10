3bad9f861032afd6dc7c6ca0c299d48b4aea9d19
Peter Tröger <peter@troeger.eu>
2021-03-10 18:36:22 +0100

    Fix test suite

--- kubeportal/tests/test_api_cluster.py
+++ kubeportal/tests/test_api_cluster.py
@@ -1,18 +1,18 @@
 import pytest
 from django.conf import settings
 
-from kubeportal.api.views import ClusterInfoView
+from kubeportal.api.views import InfoDetailView
 
 
 @pytest.mark.django_db
 def test_cluster_denied(api_client_anon):
-    for stat in ClusterInfoView.stats.keys():
+    for stat in InfoDetailView.stats.keys():
         response = api_client_anon.get(f'/api/{settings.API_VERSION}/cluster/{stat}/')
         assert response.status_code == 401
 
 
 def test_cluster(api_client):
-    for stat in ClusterInfoView.stats.keys():
+    for stat in InfoDetailView.stats.keys():
         response = api_client.get(f'/api/{settings.API_VERSION}/cluster/{stat}/')
         assert 200 == response.status_code
         data = response.json()
