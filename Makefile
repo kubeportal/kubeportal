cd9fbb1641c31101946d8903c351fc6c6a94337e
Peter Tröger <peter@troeger.eu>
2021-03-18 07:29:43 +0100

    Bump version: 0.6.0 → 0.6.1

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 SHELL=/bin/bash
-VERSION=0.6.0
+VERSION=0.6.1
 
 .PHONY: docs
 
--- kubeportal/settings.py
+++ kubeportal/settings.py
@@ -6,7 +6,7 @@ from kubeportal.secret import get_secret_key
 
 
 class Common(Configuration):
-    VERSION = '0.6.0'
+    VERSION = '0.6.1'
     API_VERSION = 'v2.0.0'
 
     SITE_ID = 1
