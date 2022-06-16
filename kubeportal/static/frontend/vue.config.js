373ba6f3570f0524d5583a6de076746e44102a52
Peter Tröger <peter@troeger.eu>
2022-06-16 11:13:18 +0200

    Save WIP

--- kubeportal/static/frontend/vue.config.js
+++ kubeportal/static/frontend/vue.config.js
@@ -1,13 +1,15 @@
-const webpack = require('webpack')
+const path = require("path");
+const webpack = require('webpack');
+
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/static/frontend/' : 'http://127.0.0.1:8086',
+  outputDir: path.resolve(__dirname, "../frontend_dist"),
+  indexPath: '../../templates/vue-generated.html',
+
   configureWebpack: {
     performance: { hints: false },
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env.VUE_APP_BASE_URL': JSON.stringify(process.env.VUE_APP_BASE_URL)
-      })
-    ]
   },
+
   devServer: {
     overlay: {
       warnings: false,
@@ -17,5 +19,13 @@ module.exports = {
     disableHostCheck: true,
     port: 8086,
     host: '0.0.0.0'
+  },
+
+  chainWebpack: config => {
+      /*
+      Check for explanations: https://github.com/EugeneDae/django-vue-cli-webpack-demo
+      */
+      config.devServer
+          .writeToDisk(filePath => filePath.endsWith('index.html'));
   }
 }
