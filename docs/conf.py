9d333b495ab61790f2c1b1a4d1df8db76195c9b5
Peter Tröger <peter@troeger.eu>
2022-06-16 11:27:10 +0200

    Bump version: 0.6.13 → 0.6.14

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 project = 'KubePortal'
 copyright = '2019, Peter Tröger'
 author = 'Peter Tröger'
-release = 'v0.6.13'
+release = 'v0.6.14'
 extensions = [
 	'sphinx_issues',
     'sphinx.ext.autosectionlabel'
